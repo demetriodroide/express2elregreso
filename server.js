c73a0f2cd8d6de2e684a0ea620e2108163485c18
demetrio <demeuforia@gmail.com>
2020-11-03 21:32:16 +0100

    muy mal

--- server.js
+++ server.js
@@ -13,36 +13,36 @@ server.use(bodyParser.json());
 
 //////////////////////////////////
 // inicializar firebase
+
+function init(){
+  let firebaseConfig = {
+    apiKey: "AIzaSyCfoLHOoi8t_gjhFcDDBIjadtxqF3NuHQQ",
+    authDomain: "el-formu.firebaseapp.com",
+    databaseURL: "https://el-formu.firebaseio.com",
+    projectId: "el-formu",
+    storageBucket: "el-formu.appspot.com",
+    messagingSenderId: "40809937070",
+    appId: "1:40809937070:web:63d6636fd38bd5df9cd53f"
+  };
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
+  }
+  init();  
+
 // base de datos por defecto
-let defaultDatabase = firebase.database();
+let Firebase = require('firebase');
+let defaultDatabase = Firebase.database();
 // ruta a la base de datos
 let noticiasRef = defaultDatabase.ref("noticias");
 
-
 ////////////////////////////////
-function init(){
-var firebaseConfig = {
-  apiKey: "AIzaSyCfoLHOoi8t_gjhFcDDBIjadtxqF3NuHQQ",
-  authDomain: "el-formu.firebaseapp.com",
-  databaseURL: "https://el-formu.firebaseio.com",
-  projectId: "el-formu",
-  storageBucket: "el-formu.appspot.com",
-  messagingSenderId: "40809937070",
-  appId: "1:40809937070:web:63d6636fd38bd5df9cd53f"
-};
-// Initialize Firebase
-firebase.initializeApp(firebaseConfig);
-}
-init();
-
-
 
 /////////////////////////////////
 // recoge el objeto de firebase y lo mete en la variable noticias
 
 noticiasRef.on('value', function(snapshot) {
     let noticias = Object.values( snapshot.val() );
-  
+  });
 /*
 const noticias = [
     {
@@ -67,26 +67,13 @@ const noticias = [
     ];
 */
 
-    //////////////////////
-
     //apiresty
 
     server.get('/loadNoticias', (req, res) => {
 
-        //codigo de firebase neceasrio 
+        //codigo de firebase necesario 
 
         res.send(noticias);
     });
 
     server.listen(listenPort, () => console.log(`Server listening on ${listenPort}`));
-
-///////////////////
-/*
-// base de datos por defecto
-let defaultDatabase = firebase.database();
-// ruta a la base de datos
-let noticiasRef = defaultDatabase.ref("noticias");
-
-noticiasRef.on('value', function(snapshot) {
-  let noticias = Object.values( snapshot.val() );
-*/
